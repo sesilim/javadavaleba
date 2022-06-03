8ff0a5fe3c881c4a1da5a460ff96b54ae212c8a6
sesilim <meboniasesili@gmail.com>
2022-06-03 18:34:12 +0400

    ss

---
+++ index.js
@@ -0,0 +1,13 @@
+var kavea = {
+    languages:["ქართული", "ინგლისური", "რუსული"]
+}
+movies: [
+    {name:"Doctor Strange" ,imdb:7},
+    {name:"Spider Man" ,imdb:7.5},
+    {name:"Vikings",imdb:5.7},
+    {name:"Ukrainian Days",imdb:6.5}
+]
+document.write("movies to watch at the moment")
+for(var i=0; i<kavea.languages.length; i++){
+    document.write(country.languages[i]);
+}
