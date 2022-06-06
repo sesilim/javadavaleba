2f87e055c53b4409bb20d64ec7c1a7c155d7ca57
sesilim <meboniasesili@gmail.com>
2022-06-06 18:11:11 +0400

    ss

--- js.js/js.js
+++ js.js/js.js
@@ -1,5 +1,5 @@
 var cinema={
-    name: [ "კავეა(სითი მოლი)", "ამირანი", "კავეა(ისთ ფოინთი)" ],
+    name: "კავეა(სითი მოლი)",
     language: ["ქართული", "ინგლისური", "რუსული"],
     movies: [
         {name:"ფილმი:Doctor Strange" ,imdb:7},
@@ -8,4 +8,18 @@ var cinema={
         {name:"ფილმი:Ukrainian Days",imdb:6.5}
     ]
 }
-
+  //  document.write("ენები");
+  //  for(var i=0; i<cinema.language.length; i++){
+    //    document.write(cinema.language[i]);
+  //  }
+   //     document.write("მიმდინარე კვირის ფილმები რეიტინგი");
+     //   for(var i=0; i<cinema.movies.length; i++){
+     //       document.write(cinema.movies[i].name + cinema.movies[i].imdb);
+    //    }
+      //  document.write("რა ენაზე გადის ფილმები სხვადასხვა კინოთეატრებში");
+      //  for(var i=0; i<cinema.name.length; i++)
+      //  document.write(cinema.name[i] + cinema.language[i]);
+document.getElementById("test1").innerHTML =(cinema.name + "</br>" + cinema.language + "</br>" +cinema.movies[0].name + "</br>" + cinema.movies[0].imdb);
+document.getElementById("test2").innerHTML =(cinema.name + "</br>" + cinema.language + "</br>" +cinema.movies[1].name + "</br>" + cinema.movies[1].imdb);
+document.getElementById("test3").innerHTML =(cinema.name + "</br>" + cinema.language + "</br>" +cinema.movies[2].name + "</br>" + cinema.movies[2].imdb);
+document.getElementById("test4").innerHTML =(cinema.name + "</br>" + cinema.language + "</br>" +cinema.movies[3].name + "</br>" + cinema.movies[3].imdb);
