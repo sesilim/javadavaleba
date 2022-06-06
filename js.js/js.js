dbe347767576fe2a47c74d2d2d8150563044553f
sesilim <meboniasesili@gmail.com>
2022-06-06 13:51:09 +0400

    ss

--- js.js/js.js
+++ js.js/js.js
@@ -2,8 +2,8 @@ var days=[
     "ორშაბათი", "სამშაბათი", "ოთხშაბათი", "ხუთშაბათი", "პარასკევი", "შაბათი", "კვირა"
 ]
 var hours=[
-    1,2,3,4,5,6,7,8,9,10
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
 ]
 var mydate= new Date();
-var date=days[mydate.getDays()]  + hours[mydate.mygetHours()];
+var date=days[mydate.getDay()]  + hours[mydate.getHours()];
 console.log(date)
